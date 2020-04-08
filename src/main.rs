d59e493ef84439ec91ffa183865610fb3180290b
Abdul Jabbar Azam <ajazam@gmail.com>
2020-04-08 19:13:43 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+pub fn text( a: &str, b : &str) -> String {
+    let mut result = String::new();
+    result.push_str(a);
+    result.push_str(b);
+    result
+}
+
+fn main() {
+    println!("{}", text("hello ", "world"));
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(text("hello ", "world"), "hello world")
+    }
+}
